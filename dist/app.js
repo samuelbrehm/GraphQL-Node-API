7bc7d6da0b33eecf168748d257e3d8ac147d1a52
samuelbrehm <samuelbporto@gmail.com>
2018-07-22 01:54:08 -0300

    Configurando o express

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
-console.log('Hello Word!');
-let a = 'Hello';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+class App {
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+    middleware() {
+        this.express.use('/hello', (req, res, next) => {
+            res.send({
+                hello: 'Hello World!'
+            });
+        });
+    }
+}
+exports.default = new App().express;
