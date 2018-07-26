e6ea72032ff3380216f8cb931a0de8c9456797a5
Samuel Brehm Porto <samuelbrehmporto@MacBook-Pro-de-Samuel.local>
2018-07-26 17:24:03 -0300

    CommentModel

--- src/interfaces/ModelsInterface.ts
+++ src/interfaces/ModelsInterface.ts
@@ -1,7 +1,9 @@
-import { UserModel } from './../models/UserModel';
-import { PostModel } from './../models/PostModels';
+import { CommentModel } from './../models/CommentModel';
+import { UserModel } from '../models/UserModel';
+import { PostModel } from '../models/PostModel';
 
 export interface ModelsInterface {
+  Comment: CommentModel;
   Post: PostModel;
   User: UserModel;
 }
