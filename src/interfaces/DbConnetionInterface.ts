ee973125e9c1be4da5770a6b7668026093f10d35
Samuel Brehm Porto <samuelbrehmporto@MacBook-Pro-de-Samuel.local>
2018-07-26 11:19:50 -0300

    UserModel

--- src/interfaces/DbConnetionInterface.ts
+++ src/interfaces/DbConnetionInterface.ts
@@ -2,6 +2,6 @@ import * as Sequelize from "sequelize";
 
 import { ModelsInterface } from "./ModelsInterface";
 
-export interface DbConnetion extends ModelsInterface{
+export interface DbConnetion extends ModelsInterface {
   sequelize: Sequelize.Sequelize;
 }
