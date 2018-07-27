dda51271a28a8bebf2a4d687bb874e621661c0ee
Samuel Brehm Porto <samuelbrehmporto@MacBook-Pro-de-Samuel.local>
2018-07-27 00:12:31 -0300

    Modularizando nosso schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,45 +1,15 @@
 import { makeExecutableSchema } from 'graphql-tools';
 
-const users: any[] = [
-  {
-    id: 1,
-    name: 'John',
-    email: 'john@email.com',
-  },
-  {
-    id: 2,
-    name: 'Dany',
-    email: 'dany@email.com',
-  },
-];
+import { Query } from './query';
+import { Mutation } from './mutation';
 
-const typeDefs = `
-    type User {
-        id: ID!
-        name: String!
-        email: String
-    }
-
-    type Query {
-        allUsers: [User!]!
-    }
-
-    type Mutation {
-        createUser(name: String!, email: String!): User
-    }
+const SchemaDefinition = `
+  type Schema {
+    query: Query
+    mutation: Mutation
+  }
 `;
 
-const resolvers = {
-  Query: {
-    allUsers: () => users
-  },
-  Mutation: {
-    createUser: (parent, args) => {
-      const newUser = Object.assign({ id: users.length + 1 }, args);
-      users.push(newUser);
-      return newUser;
-    }
-  }
-};
-
-export default makeExecutableSchema({ typeDefs, resolvers });
+export default makeExecutableSchema({
+  typeDefs: [SchemaDefinition, Query, Mutation]
+});
