a42549523db01096cd756f22d8d069c725216a25
Samuel Brehm Porto <samuelbrehmporto@MacBook-Pro-de-Samuel.local>
2018-07-28 22:30:54 -0300

    Post schema: types, queries e mutations

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -4,6 +4,7 @@ import { Query } from './query';
 import { Mutation } from './mutation';
 
 import { userTypes } from './resources/user/user.schema';
+import { postTypes } from './resources/post/post.schema';
 
 const SchemaDefinition = `
   type Schema {
@@ -13,5 +14,5 @@ const SchemaDefinition = `
 `;
 
 export default makeExecutableSchema({
-  typeDefs: [SchemaDefinition, Query, Mutation, userTypes]
+  typeDefs: [SchemaDefinition, Query, Mutation, postTypes, userTypes]
 });
