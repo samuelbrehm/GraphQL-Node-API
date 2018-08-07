de11b3d73f6d06c236154afc60600d575217fafc
Samuel Brehm Porto <samuelbrehm@gmail.com>
2018-08-07 16:05:40 -0300

    Primeiro teste com a camada de autenticação

--- dist/app.js
+++ dist/app.js
@@ -4,14 +4,14 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const models_1 = require("./models");
 const schema_1 = require("./graphql/schema");
+const extract_jwt_middleware_1 = require("./middlewares/extract-jwt.middleware");
 class App {
     constructor() {
         this.express = express();
         this.middleware();
     }
     middleware() {
-        this.express.use('/graphql', (req, res, next) => {
-            req['context'] = {};
+        this.express.use('/graphql', extract_jwt_middleware_1.extractJwtMiddleware(), (req, res, next) => {
             req['context'].db = models_1.default;
             next();
         }, graphqlHTTP(req => ({
---
+++ dist/middlewares/extract-jwt.middleware.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jwt = require("jsonwebtoken");
+const models_1 = require("./../models");
+const utils_1 = require("../utils/utils");
+exports.extractJwtMiddleware = () => {
+    return (req, res, next) => {
+        let authorization = req.get('authorization');
+        let token = authorization ? authorization.split(' ')[1] : undefined;
+        req['context'] = {};
+        req['context']['authorization'] = authorization;
+        if (!token) {
+            return next();
+        }
+        jwt.verify(token, utils_1.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return next();
+            }
+            models_1.default.User.findById(decoded.sub, {
+                attributes: ['id', 'email']
+            }).then((user) => {
+                if (user) {
+                    req['context']['user'] = {
+                        id: user.get('id'),
+                        email: user.get('email'),
+                    };
+                }
+                return next();
+            });
+        });
+    };
+};
