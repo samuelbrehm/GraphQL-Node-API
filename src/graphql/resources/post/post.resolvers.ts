8e994a9de1c36146c5b5ddf5754d3740fb8348f7
Samuel Brehm Porto <samuelbrehm@gmail.com>
2018-08-09 09:26:51 -0300

    Comment resolvers: adicionando autenticação

--- src/graphql/resources/post/post.resolvers.ts
+++ src/graphql/resources/post/post.resolvers.ts
@@ -2,7 +2,10 @@ import { DbConnection } from '../../../interfaces/DbConnectionInterface';
 import { GraphQLResolveInfo } from 'graphql';
 import { PostInstance } from '../../../models/PostModel';
 import { Transaction } from '../../../../node_modules/@types/sequelize';
-import { handleError } from '../../../utils/utils';
+import { handleError, throwError } from '../../../utils/utils';
+import { compose } from "../../composable/composable.resolver";
+import { authResolvers } from "../../composable/auth.resolver";
+import { AuthUser } from "../../../interfaces/AuthUserInterface";
 
 export const postResolvers = {
   Post: {
@@ -21,17 +24,19 @@ export const postResolvers = {
 
   Query: {
     posts: (parent, { first = 10, offset = 0 }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
-      return db.Post.findAll({
-        limit: first,
-        offset: offset
-      }).catch(handleError);
+      return db.Post
+        .findAll({
+          limit: first,
+          offset: offset
+        }).catch(handleError);
     },
 
     post: (parent, { id }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
       id = parseInt(id);
-      return db.Post.findById(id)
+      return db.Post
+        .findById(id)
         .then((post: PostInstance) => {
-          if (!post) throw new Error(`Post with id ${id} not found!`);
+          throwError(!post, `Post with id ${id} not found!`);
           return post;
         })
         .catch(handleError);
@@ -39,36 +44,47 @@ export const postResolvers = {
   },
 
   Mutation: {
-    createPost: (parent, { input }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
+    createPost: compose(...authResolvers)((parent, { input }, { db, authUser }: { db: DbConnection, authUser: AuthUser }, info: GraphQLResolveInfo) => {
+      input.author = authUser.id;
       return db.sequelize
         .transaction((t: Transaction) => {
-          return db.Post.create(input, { transaction: t });
+          return db.Post
+            .create(input, { transaction: t });
         })
         .catch(handleError);
-    },
+    }),
 
-    updatePost: (parent, { id, input }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
+    updatePost: compose(...authResolvers)((parent, { id, input }, { db, authUser }: { db: DbConnection, authUser: AuthUser }, info: GraphQLResolveInfo) => {
       id = parseInt(id);
       return db.sequelize
         .transaction((t: Transaction) => {
-          return db.Post.findById(id).then((post: PostInstance) => {
-            if (!post) throw new Error(`Post with id ${id} not found!`);
-            return post.update(input, { transaction: t });
-          });
+          return db.Post
+            .findById(id)
+            .then((post: PostInstance) => {
+              throwError(!post, `Post with id ${id} not found!`);
+              throwError(post.get('author') !== authUser.id, `Unauthorized! You can only edit posts by yourself!`);
+              input.author = authUser.id;
+              return post.update(input, { transaction: t });
+            });
         })
         .catch(handleError);
-    },
+    }),
 
-    deletePost: (parent, { id }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
+    deletePost: compose(...authResolvers)((parent, { id }, { db, authUser }: { db: DbConnection, authUser: AuthUser }, info: GraphQLResolveInfo) => {
       id = parseInt(id);
       return db.sequelize
         .transaction((t: Transaction) => {
-          return db.Post.findById(id).then((post: PostInstance) => {
-            if (!post) throw new Error(`Post with id ${id} not found!`);
-            return post.destroy({ transaction: t }).then(post => !!post);
-          });
+          return db.Post
+            .findById(id)
+            .then((post: PostInstance) => {
+              throwError(!post, `Post with id ${id} not found!`);
+              throwError(post.get('author') !== authUser.id, `Unauthorized! You can only delete posts by yourself!`);
+              return post
+                .destroy({ transaction: t })
+                .then(post => !!post);
+            });
         })
         .catch(handleError);
-    }
+    })
   }
 };
